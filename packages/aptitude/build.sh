5fc7459bade6c5d2db1118ff8544cd97b3967bfd
Pierre Rudloff <contact@rudloff.pro>
2016-06-13 21:44:57 +0200

    WIP

---
+++ packages/aptitude/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://aptitude.alioth.debian.org/doc/en/
+TERMUX_PKG_DESCRIPTION="Terminal-based package manager"
+TERMUX_PKG_VERSION=0.8.1
+TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/a/aptitude/aptitude_${TERMUX_PKG_VERSION}.orig.tar.xz
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_FOLDERNAME=aptitude-${TERMUX_PKG_VERSION}
+TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
