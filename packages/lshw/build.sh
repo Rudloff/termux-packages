d11c8a7a3bb2ebdddb38e3cd431dbfc84263c170
Pierre Rudloff <contact@rudloff.pro>
2016-06-15 21:22:20 +0200

    WIP

---
+++ packages/lshw/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://rtmpdump.mplayerhq.hu/
+TERMUX_PKG_DESCRIPTION="Information about hardware configuration"
+TERMUX_PKG_VERSION=02.18
+TERMUX_PKG_SRCURL=http://www.ezix.org/software/files/lshw-B.${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_FOLDERNAME=lshw-B.${TERMUX_PKG_VERSION}
+TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
