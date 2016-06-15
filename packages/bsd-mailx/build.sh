475c0a945dfab10c0abdff4a96c082c9218c5d63
Pierre Rudloff <contact@rudloff.pro>
2016-06-15 21:38:39 +0200

    WIP

---
+++ packages/bsd-mailx/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://rtmpdump.mplayerhq.hu/
+TERMUX_PKG_DESCRIPTION="Simple mail user agent"
+TERMUX_PKG_VERSION=8.1.2-0.20160123cvs
+TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/b/bsd-mailx/bsd-mailx_${TERMUX_PKG_VERSION}.orig.tar.bz2
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_FOLDERNAME=bsd-mailx-${TERMUX_PKG_VERSION}.orig
+TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
