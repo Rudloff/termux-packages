b27b24222de99b4b15058113f95b793d48aca604
Pierre Rudloff <contact@rudloff.pro>
2016-10-13 00:43:10 +0200

    WIP

---
+++ packages/libnotify/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/libnotify/
+TERMUX_PKG_MAJOR_VERSION=0.7
+TERMUX_PKG_VERSION=${TERMUX_PKG_MAJOR_VERSION}.6
+TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/libnotify/{$TERMUX_PKG_MAJOR_VERSION}/libnotify-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_DEPENDS="glib, gdk-pixbuf"
