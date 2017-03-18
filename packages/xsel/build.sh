509de96ba11e65f6539d1547938595162c8ed062
Pierre Rudloff <contact@rudloff.pro>
2017-03-18 01:26:23 +0100

    WIP xsel

---
+++ packages/xsel/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=http://www.vergenet.net/~conrad/software/xsel/
+TERMUX_PKG_DESCRIPTION="Command-line tool to access X clipboard and selection buffers"
+TERMUX_PKG_VERSION=1.2.0
+TERMUX_PKG_SRCURL=http://www.vergenet.net/~conrad/software/xsel/download/xsel-${TERMUX_PKG_VERSION}.tar.gz
+#TERMUX_PKG_BUILD_IN_SRC=yes
+#TERMUX_PKG_FOLDERNAME=wine-${TERMUX_PKG_VERSION}
+#TERMUX_PKG_DEPENDS="ncurses, libpng"
+TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
+#TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-wine-tools=${TERMUX_PKG_HOSTBUILD_DIR} --without-x --without-freetype --without-pcap"
